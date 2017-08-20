6855486bc84a46bf0e8985d0007d4a699a150c70
Nika Jones <njones-git@nikajon.es>
2017-08-19 22:51:33 -0700

    Added a base58check option to the command

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"io"
@@ -9,16 +11,25 @@ import (
 	"os"
 )
 
+func checkSum(b []byte) []byte {
+	sh1, sh2 := sha256.New(), sha256.New()
+	sh1.Write(b)
+	sh2.Write(sh1.Sum(nil))
+	return sh2.Sum(nil)
+}
+
 func main() {
 	var (
 		err error
 		bin []byte
 
-		help    = flag.Bool("h", false, "display this message")
-		lnBreak = flag.Int("b", 76, "break encoded string into num character lines. Use 0 to disable line wrapping")
-		input   = flag.String("i", "-", `input file (use: "-" for stdin)`)
-		output  = flag.String("o", "-", `output file (use: "-" for stdout)`)
-		decode  = flag.Bool("d", false, `decode input`)
+		help     = flag.Bool("h", false, "display this message")
+		lnBreak  = flag.Int("b", 76, "break encoded string into num character lines. Use 0 to disable line wrapping")
+		input    = flag.String("i", "-", `input file (use: "-" for stdin)`)
+		output   = flag.String("o", "-", `output file (use: "-" for stdout)`)
+		decode   = flag.Bool("d", false, `decode input`)
+		check    = flag.Bool("k", false, `use sha256 check`)
+		useError = flag.Bool("e", false, `write error to stderr`)
 	)
 
 	flag.Parse()
@@ -54,11 +65,35 @@ func main() {
 			fmt.Fprintf(os.Stderr, "decode input err: %v\n", err)
 			os.Exit(1)
 		}
+
+		var checkResult bool
+		if *check {
+			chk := len(decoded) - 4
+			decodedCk := decoded[chk:]
+			decoded = decoded[:chk]
+			sum := checkSum(decoded)
+			checkResult = hex.EncodeToString(sum[:4]) == hex.EncodeToString(decodedCk)
+		}
+
 		io.Copy(fout, bytes.NewReader(decoded))
+
+		if *check && !checkResult {
+			if *useError {
+				fmt.Fprintf(os.Stderr, "%t", false)
+			}
+			os.Exit(3)
+		}
+
 		os.Exit(0)
 	}
 
+	if *check {
+		sum := checkSum(bin)
+		bin = append(bin, sum[:4]...)
+	}
+
 	encoded := FastBase58Encoding(bin)
+
 	if *lnBreak > 0 {
 		lines := (len(encoded) / *lnBreak) + 1
 		for i := 0; i < lines; i++ {
